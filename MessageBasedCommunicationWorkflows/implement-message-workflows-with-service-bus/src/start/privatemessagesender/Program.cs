a4e139731029bf97d166dd179cb0fb6fdd6d3063
MedNidhalSamet <nsamet21@gmail.com>
2024-05-29 12:51:23 +0200

    Send and receive messages by using a queue

---
+++ MessageBasedCommunicationWorkflows/implement-message-workflows-with-service-bus/src/start/privatemessagesender/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+
+namespace privatemessagesender
+{
+    class Program
+    {
+        const string ServiceBusConnectionString = "";
+        const string QueueName = "salesmessages";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sending a message to the Sales Messages queue...");
+            SendSalesMessageAsync().GetAwaiter().GetResult();
+            Console.WriteLine("Message was sent successfully.");
+        }
+
+        static async Task SendSalesMessageAsync()
+        {
+            // Create a Service Bus client
+            await using var client = new ServiceBusClient(ServiceBusConnectionString);
+
+            // Create a sender
+            await using ServiceBusSender sender = client.CreateSender(QueueName);
+            try
+            {
+                // Create and send a message
+                string messageBody = $"$10,000 order for bicycle parts from retailer Adventure Works.";
+                var message = new ServiceBusMessage(messageBody);
+                Console.WriteLine($"Sending message: {messageBody}");
+                await sender.SendMessageAsync(message);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"{DateTime.Now} :: Exception: {exception.Message}");
+            }
+            finally
+            {
+                // Close the connection to the sender
+                await sender.DisposeAsync();
+                await client.DisposeAsync();
+            }
+        }
+    }
+}
